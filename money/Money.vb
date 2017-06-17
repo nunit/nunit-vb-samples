44e2ec5df0dfab794e10413e7e39310f826a9517
Chris Maddock <chrismaddock@live.co.uk>
2017-06-17 15:11:08 +0100

    Remove outdated copyright notices

--- money/Money.vb
+++ money/Money.vb
@@ -1,9 +1,3 @@
-' ****************************************************************
-' This is free software licensed under the NUnit license. You
-' may obtain a copy of the license as well as information regarding
-' copyright ownership at http://nunit.org
-' ****************************************************************
-
 Option Explicit On 
 
 Namespace NUnit.Samples
